886f2cc57a7f74cb80ad68f16008e327b0da6119
abdikeem <abdihakimabdirahman21@mail.com>
2021-12-05 17:27:46 +0300

    Header.css

--- src/Header.js
+++ src/Header.js
@@ -3,12 +3,12 @@ import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
 
-
 function Header() {
     return (
         <div className="header">
-            <PersonIcon />
-            <ForumIcon />
+            <PersonIcon className="header_icon" fontSize="large"/>
+            <img className="header_logo" src="https://cdn.worldvectorlogo.com/logos/tinder-2.svg" alt="tinder_logo" />
+            <ForumIcon className="header_icon" fontSize="large"/>
         </div>
     )
 }
