17ecf3822b979f9d69d41f293dcfb2629f72d830
abdikeem <abdihakimabdirahman21@mail.com>
2021-12-05 18:28:28 +0300

    TinderCards

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
+import TinderCard from 'react-tinder-card';
 
 function TinderCards() {
+    const [people, setPeople] = useState([]);
+    // Push to an array in react
+    // setPeople([...people, 'Hashim', 'yasir])
+
+    {
+        name: 'Richard Hendricks',
+        // url: ''
+    }
     return (
         <div>
-            
+            {people.map(person => (
+                <TinderCard>
+                    <div className="card" style={{backgroundImage: `url(${person.url})`}}>
+                        <h3>{person.name}</h3>
+                    </div>
+                </TinderCard>
+            ))}
         </div>
     )
 }
